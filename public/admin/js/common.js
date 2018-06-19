c40fc7595d8a64758acecac2c2355fd68d967091
xuhongying <857921487@qq.com>
2018-06-19 12:31:31 +0800

    基本框架完成

---
+++ public/admin/js/common.js
@@ -0,0 +1,59 @@
+//所有页面都有进度条效果
+
+//注册一个全局的ajaxStart事件，所有的ajax在开启的时候，会触发这个事件
+$(document).ajaxStart(function () {
+  //开启进度条
+  NProgress.start();
+});
+$(document).ajaxStop(function () {
+  //关闭进度条
+NProgress.done();
+});
+
+//非登陆页面，判断当前用户是否是登录了，如果登录了，就继续，如果没登陆，需要跳转到登录页面。
+if(location.href.indexOf("login.html") == -1){
+  $.ajax({
+    type:"get",
+    url:"/employee/checkRootLogin",
+    success:function (data) {
+      if(data.error === 400){
+        //说明用户没有登录，跳转到登录页面
+        location.href = "login.html";
+      }
+    }
+  })
+}
+
+//二级分类显示隐藏功能
+//点击a标签，让a下面的div显示隐藏
+$('.child').prev().on('click',function () {
+  $(this).next().slideToggle();
+  });
+
+
+  //侧边栏显示隐藏功能
+  $('.icon_menu').on('click',function () { 
+    $('.lt_aside').toggleClass('now');
+    $('.lt_main').toggleClass('now');
+   });
+
+   //点击退出，显示模态框
+   $('.icon_logout').on('click',function () { 
+     //console.log(111)
+     $('#logoutModal').modal('show');
+    });
+
+    //给确定按钮注册点击事件
+    $('.btn_logout').on('click',function () { 
+      $.ajax({
+        type:'get',
+        url:'/employee/employeeLogout',
+        success:function (info) { 
+          if(info.success){
+            location.href = 'login.html';
+          }
+         }
+      })
+     });
+
+   
---
+++ public/admin/js/login.js
@@ -0,0 +1,100 @@
+$(function () { 
+  //表单校验的功能
+  //1. 用户名不能为空
+  //2. 用户密码不能为空
+  //3. 用户密码的长度是6-12位
+
+  //如何使用表单校验插件：
+  //1. 引包
+  //2. 调用bootstrapValidator
+  $('form').bootstrapValidator({
+    // 指定校验时的图标显示，默认是bootstrap风格
+  feedbackIcons: {
+    //使用bootstrap 组建中的Glyphicons 字体图标
+    valid: 'glyphicon glyphicon-ok', //正确时√
+    invalid: 'glyphicon glyphicon-remove',//错误时×
+    validating: 'glyphicon glyphicon-refresh'//更新时
+  },
+
+    //设置校验规则
+    fields:{
+       //校验用户名，对应表单的name属性
+  
+      username:{
+        validators:{
+          //用户名不能为空
+          notEmpty:{
+            message:'用户名不能为空'
+          },
+          stringLength:{
+            min:3,
+            max:9,
+            message:'用户名在3到9位之间'
+          },
+          callback:{
+            message:'用户名不存在'
+              
+          }
+          
+
+        }
+      },
+      //密码校验规则
+      password:{
+        validators:{
+          //密码不能为空
+          notEmpty:{
+            message:'密码不能为空'
+          },
+          stringLength:{
+            min:6,
+            max:12,
+            message:'密码必须在6到12位之间'
+          },
+          callback:{
+            message:'密码错误'
+              
+          }
+          
+
+        }
+      }
+    }
+  });
+  //注册表单成功事件，当表单注册成功时触发，阻止浏览器的自动跳转，用ajax提交数据跳转页面
+  $('form').on('success.form.bv',function (e) { 
+      //阻止页面跳转
+      e.preventDefault();
+      $.ajax({
+        type:'post',
+        url:'/employee/employeeLogin',
+        data:$('form').serialize(),
+        success:function (info) { 
+          if(info.success){
+            location.href = 'index.html'
+          }
+          if(info.error===1000){
+             //手动调用方法，updateStatus让username校验失败即可
+          //第一个参数：改变哪个字段
+          //第二个参数：改成什么状态  VALID:通过  INVALID:不通过
+          //第三个参数：选择提示的信息
+             //获取表单校验实例//使用表单校验实例可以调用一些常用的方法。
+            $("form").data('bootstrapValidator').updateStatus('username','INVALID','callback');
+          }
+          if(info.error === 1001){
+            $("form").data('bootstrapValidator').updateStatus('password','INVALID','callback');
+          }
+         }
+      });
+       //重置功能，重置样式  
+       //当点击重置按钮时，重置表单的样式和内容
+       $('[type="reset"]').on('click',function () {
+        //获取表单校验实例//使用表单校验实例可以调用一些常用的方法。
+        $("form").data('bootstrapValidator').resetForm(true);
+
+         })
+   
+
+   });
+
+ })
