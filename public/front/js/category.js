cbd241ac624e3abc84f92cda6d4800f1d39fd43a
xuhongying <857921487@qq.com>
2018-06-21 11:31:45 +0800

    分类页

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -7,12 +7,25 @@ $(function () {
     success: function (info) {
       console.log(info);
       $('.lt_category_l ul').html(template('firstTpl', info));
-      
+
  //获取到一级分类之后，渲染了第一个一级分类对应的二级分类
       renderSecond(info.rows[0].id)
     }
   })
 
+  //点击一级分类显示对应的二级分类
+  $('.lt_category_l').on('click','li',function () { 
+    $(this).addClass('now').siblings().removeClass('now');
+    //2. 渲染了指定的二级分类
+    var id= $(this).data('id');
+    renderSecond(id);
+    
+    // 让右边的滚动容器滚到0，0的位置
+
+    mui('.lt_category_r .mui-scroll-wrapper').scroll().scrollTo(0,0,1000);//1000毫秒滚动到顶
+   })
+
+  //渲染二级分类
   function renderSecond(id) { //id 为一级分类的id，知道渲染哪一个一级分类对应的二级分类
     $.ajax({
       type:'get',
@@ -22,7 +35,7 @@ $(function () {
       },
       success:function (info) { 
         console.log(info);
-
+        $('.lt_category_r ul').html(template('secondTpl', info));
        }
     })
    }
