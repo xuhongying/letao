b4293e914032f89c4ef55776fdeabc46d14e3944
xuhongying <857921487@qq.com>
2018-06-25 14:30:47 +0800

    购物车完成

--- public/front/js/cart.js
+++ public/front/js/cart.js
@@ -5,7 +5,7 @@ $(function () {
 
   mui.init({
     pullRefresh : {
-      container:".lt_content",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+      container:".mui-scroll-wrapper",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
       down : {
         
         auto: true,//可选,默认false.首次加载自动下拉刷新一次
@@ -18,7 +18,7 @@ $(function () {
              
               //因为数据比较少所以模拟1s后获取到数据
               setTimeout(function () { 
-                console.log(info);
+               // console.log(info);
                 //如果用户没有登录，跳转到登录页，需要回跳
                 if(info.error){
                   location.href='login.html?back='+location.href;
@@ -27,8 +27,8 @@ $(function () {
               
                 $('#OA_task_2').html(template('tpl',{rows:info}));
                 //结束下拉刷新
-                //console.log(mui('.lt_content').pullRefresh());
-                mui('.lt_content').pullRefresh().endPulldownToRefresh();
+                //console.log(mui('.mui-scroll-wrapper').pullRefresh());
+                mui('.mui-scroll-wrapper').pullRefresh().endPulldownToRefresh();
                
                },1000)
              }
@@ -47,20 +47,23 @@ $(function () {
   $("#OA_task_2").on("tap", ".btn_delete", function () {
 
     var id = $(this).data("id");
+    //console.log(id)
     mui.confirm("你确定要删除这件商品吗？", "温馨提示", ["是", "否"], function (e) {
       if (e.index === 0) {
 
         $.ajax({
           type: 'get',
           url: '/cart/deleteCart',
+     
           data: {
             //注意点：文档需要传递一个数组
-            id: id
+            id: [id]
           },
           success: function (info) {
             if (info.success) {
               //重新下拉
               mui(".mui-scroll-wrapper").pullRefresh().pulldownLoading();
+        
             }
           }
         });
@@ -68,6 +71,72 @@ $(function () {
     })
 
   });
+//计算总金额   给复选框注册委托事件   注册change事件
+  $("#OA_task_2").on("change", ".ck", function () {
+    //console.log($('.ck'))
+    var total = 0;
+    //所有选中的checkbox
+    $('.ck:checked').each(function (i,e) {  
+      //console.log($('.ck:checked'))
+     total += $(this).data('price') * $(this).data('num');
+    })
+     //显示出来
+     $('.lt_total span').text(total.toFixed(2));
 
-  
- })
+
+  })
+
+
+  //修改功能
+
+  $("#OA_task_2").on("tap", ".btn_edit", function () {
+    //将需要的数据都存到btn_edit的自定义属性上
+
+    //获取所有的自定义属性
+    var data = this.dataset;//this是DOM对象，用Dom的方法一次性可以获取所有的比用jq的方法一个一个获取更简单
+    
+    //回显数据
+    var html = template('tpl2', data)//数据与模板相结合
+    //需要把html字符串中所有的\n给干掉
+    html = html.replace(/\n/g, '');
+    //弹出确认框
+    mui.confirm(html, '编辑商品', ['确定', '取消'], function (e) {
+      //确认框的内容是一个很复杂的，结果所以直接写html
+        if(e.index == 0){
+          var id = data.id;
+          var size = $('.proSize span.now').text();
+          var num = $('.mui-numbox-input').val();
+          console.log(num)
+          $.ajax({
+          type:'post',
+          url:'/cart/updateCart',
+          data:{
+            id:id,
+            size:size,
+            num:num
+          },
+          success:function (info) { 
+          console.log(info);
+          if(info.success){
+            //重新下拉刷新一次
+            mui(".mui-scroll-wrapper").pullRefresh().pulldownLoading();
+   
+          }
+           }
+        })
+        }
+    })
+    //让span可以选择
+      //让尺码可以选择
+      $('.proSize span').on('click',function () { 
+        $(this).addClass('now').siblings().removeClass('now');
+       })
+
+    //让数组控件可以加减
+    mui('.mui-numbox').numbox();
+
+
+
+  })
+
+})
--- public/front/js/product.js
+++ public/front/js/product.js
@@ -1,81 +1,81 @@
 
 
-$(function () { 
+$(function () {
   //获取到地址栏中的商品id，发送ajax请求，渲染动态页面
   var productId = getSearch().productId;
   $.ajax({
-    type:'get',
-    url:'/product/queryProductDetail',
-    data:{
-      id:productId
+    type: 'get',
+    url: '/product/queryProductDetail',
+    data: {
+      id: productId
     },
-    success:function (info) { 
+    success: function (info) {
       console.log(info);
       //模板与数据结合
-      $('.mui-scroll').html(template('tpl',info));
+      $('.mui-scroll').html(template('tpl', info));
 
-//       //让轮播图可以动
-        //获得slider插件对象
-    mui('.mui-slider').slider({
-          interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
-        });
+      //       //让轮播图可以动
+      //获得slider插件对象
+      mui('.mui-slider').slider({
+        interval: 1000//自动轮播周期，若为0则不自动播放，默认为0；
+      });
       //让尺码可以选择
-      $('.proSize span').on('click',function () { 
+      $('.proSize span').on('click', function () {
         $(this).addClass('now').siblings().removeClass('now');
-       })
+      })
 
       //让数字键可以加减
-     //文档中有
+      //文档中有
       mui(".mui-numbox").numbox()
-     }
+    }
   })
 
   //加入购物车的功能
   //1. 给加入购物车按钮注册点击事件
-  
+
   //2. 获取产品id，数量，尺码，校验尺码不可以为空
   //3.发送ajax请求
 
 
-$('.btn_cart').on('click',function () { 
+  $('.btn_cart').on('click', function () {
 
-var size = $('.proSize span.now').text();
+    var size = $('.proSize span.now').text();
 
-if(!size){
-  mui.toast('请选择尺码');
-  return;
-}
+    if (!size) {
+      mui.toast('请选择尺码');
+      return;
+    }
 
-var num = $('.proNum input').val();
+    var num = $('.proNum input').val();
 
 
-  $.ajax({
-    type:'post',
-    url:'/cart/addCart',
-    data:{
-      productId:productId,
-      num:num,
-      size:size
-    },
-    success:function (info) { 
-      console.log(info);
-      if(info.success){
-        mui.confirm('温馨提示','添加成功',['去购物车','继续浏览'],function(e){
-          console.log(e);
-            if(e.index ==0){
+    $.ajax({
+      type: 'post',
+      url: '/cart/addCart',
+      data: {
+        productId: productId,
+        num: num,
+        size: size
+      },
+      success: function (info) {
+        console.log(info);
+        if (info.success) {
+          mui.confirm('温馨提示', '添加成功', ['去购物车', '继续浏览'], function (e) {
+            //console.log(e);
+            if (e.index == 0) {
               location.href = 'cart.html'
             }
-        }) ;
+          });
+        }
+        if (info.error) {
+          //说明没登录,跳转到登录页面, 把当前页的地址传递到了登录页面。
+          location.href = 'login.html?back=' + location.href;
+        }
       }
-      if(info.error){
-         //说明没登录,跳转到登录页面, 把当前页的地址传递到了登录页面。
-        location.href = 'login.html?back='+location.href;
-      }
-     }
+    })
   })
- })
 
 
 
 
- })
+})
